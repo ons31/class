d3aecb1d5d875816d3c0c4407b4114c724cbec30
ons31 <32807195+ons31@users.noreply.github.com>
2017-10-27 19:45:18 +0900

    Initial Commit

---
+++ class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  class
+//
+//  Created by Jin Tanaka on 10/27/29 H.
+//  Copyright © 29 Heisei AUNQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
